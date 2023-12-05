d596b32afa23ed57a7639dc6548197bd9f5a74ae
unknown <kian1523@naver.com>
2023-12-05 15:50:53 +0900

    chore: fix schema, code smell

--- coupon-event/src/main/resources/db/schema.sql
+++ coupon-event/src/main/resources/db/schema.sql
@@ -7,7 +7,6 @@ CREATE TABLE IF NOT EXISTS promotion (
   end_date DATE,
   discount_policy VARCHAR(255)
 ) engine=InnoDB;
-drop table coupon;
 CREATE TABLE IF NOT EXISTS coupon (
   coupon_id VARCHAR(255) NOT NULL PRIMARY KEY,
   promotion_id VARCHAR(255),
@@ -16,9 +15,9 @@ CREATE TABLE IF NOT EXISTS coupon (
   during_date DATE,
   member_id VARCHAR(255),
   state VARCHAR(100),
-  INDEX coupon_promotion_member_idx(promotion_id, member_id)
+  INDEX coupon_promotion_idx(promotion_id)
 ) engine=InnoDB;
-# ALTER TABLE coupon ADD INDEX coupon_promotion_member_idx(promotion_id, member_id);
+# ALTER TABLE coupon ADD INDEX coupon_promotion_idx(promotion_id);
 
 CREATE TABLE IF NOT EXISTS member (
  member_id VARCHAR(255) NOT NULL PRIMARY KEY
