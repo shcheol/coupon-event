37b5d16a894cfe32bc62275f522fb99181d9ce92
shcheol <kian1523@naver.com>
2023-12-06 20:24:14 +0900

    chore: test는 inMemoryDB사용

--- coupon-event/src/main/resources/db/schema.sql
+++ coupon-event/src/main/resources/db/schema.sql
@@ -7,7 +7,6 @@ CREATE TABLE IF NOT EXISTS promotion (
   end_date DATE,
   discount_policy VARCHAR(255)
 ) engine=InnoDB;
-drop table coupon;
 CREATE TABLE IF NOT EXISTS coupon (
   coupon_id VARCHAR(255) NOT NULL PRIMARY KEY,
   promotion_id VARCHAR(255),
@@ -18,7 +17,6 @@ CREATE TABLE IF NOT EXISTS coupon (
   state VARCHAR(100),
   INDEX coupon_promotion_member_idx(promotion_id, member_id)
 ) engine=InnoDB;
-# ALTER TABLE coupon ADD INDEX coupon_promotion_member_idx(promotion_id, member_id);
 
 CREATE TABLE IF NOT EXISTS member (
  member_id VARCHAR(255) NOT NULL PRIMARY KEY
